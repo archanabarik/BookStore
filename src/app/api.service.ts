18783ef57dc8d0f75fc80adc3dd1d3fa35f9218a
Archana Kumari <archanakumaribarik@gmail.com>
2018-01-29 10:47:13 +0530

    Create api.service.ts

---
+++ src/app/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ApiService {
+  private baseUrl = 'http://localhost:8080/items';
+
+  constructor(private http: HttpClient) { }
+
+  getDragons$(): Observable<any[]> {
+    return this.http
+      .get(`${this.baseUrl}`, {
+        headers: new HttpHeaders().set(
+          'Authorization', `Bearer ${localStorage.getItem('access_token')}`
+        )
+      })
+      .pipe(
+        catchError(this._handleError)
+      );
+  }
+
+  private _handleError(err: HttpErrorResponse | any) {
+    const errorMsg = err.message || 'Unable to retrieve data';
+    return Observable.throw(errorMsg);
+  }
+
+}
+
