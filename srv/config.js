f6741bac5844ba9b42b8561cc0a8ac78fd8af1e6
Archana Kumari <archanakumaribarik@gmail.com>
2018-01-29 11:11:46 +0530

    Update config.js

--- srv/config.js
+++ srv/config.js
@@ -2,4 +2,4 @@ module.exports = {
   CLIENT_DOMAIN: "bookcart.auth0.com", // e.g. 'you.auth0.com'
   AUTH0_AUDIENCE: "https://bookcart.auth0.com/api/v2/",
   NAMESPACE: "http://example.com/roles"
-}
+};
